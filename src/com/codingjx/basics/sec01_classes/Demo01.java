da8399b34a7a6bf5b6ece984d89c2655c1e3fc8b
chiahsing24 <chiahsingtony@gmail.com>
2022-08-01 16:22:52 -0700

    Primitive variables: integer

---
+++ src/com/codingjx/basics/sec01_classes/Demo01.java
@@ -0,0 +1,33 @@
+package com.codingjx.basics.sec01_classes;
+
+class Student {
+    int id = 1000;
+
+    byte age = 18;
+    //    byte rank = 165; // [-128, 127]
+    byte rank = 127;
+    short rank_s = 165;
+//    int phone = 2234567890; // interger number too large
+//    int phone = 2234567890L; //  imcompatible type
+    long phone_l = 223_456_7890L;
+
+    int minValue = Integer.MIN_VALUE;
+    int maxValue = Integer.MAX_VALUE;
+
+    void compute() {
+        int nextId = id + 1;
+        System.out.println("id: " + id);
+        System.out.println("nextId: " + nextId);
+        System.out.println("age: " + age);
+        System.out.println("phone: " + phone_l);
+        System.out.println("minValue: " + minValue);
+        System.out.println("maxValue: " + maxValue);
+    }
+}
+
+public class Demo01 {
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.compute();
+    }
+}
