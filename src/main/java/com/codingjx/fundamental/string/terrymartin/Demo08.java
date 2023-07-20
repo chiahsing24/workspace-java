18e5aedfa1cc7aa151e1fa153422350ac07fbc05
chiahsingchu <chiahsingtony@gmail.com>
2023-07-19 20:39:19 -0700

    Add more string methods demo

---
+++ src/main/java/com/codingjx/fundamental/string/terrymartin/Demo08.java
@@ -0,0 +1,9 @@
+package com.codingjx.fundamental.string.terrymartin;
+
+public class Demo08 {
+    public static void main(String[] args) {
+        String myText = "Four score and seven years ago";
+        System.out.println(myText.contains("seven"));
+
+    }
+}
