7f0c3abee32778cf702598f75739416799674e72
chiahsingchu <chiahsingtony@gmail.com>
2023-08-12 23:09:10 -0700

    Add numeric wrapper class example

---
+++ src/main/java/com/codingjx/fundamental/number/Demo05.java
@@ -0,0 +1,25 @@
+package com.codingjx.fundamental.number;
+
+public class Demo05 {
+    public static void main(String[] args) {
+        int num1 = 7;
+        Integer num1B = Integer.valueOf(num1);
+        Double myDouble = Double.valueOf(123.123);
+        Float myFloat = Float.valueOf(23.23f);
+        Byte myByte = Byte.valueOf("23");
+
+        System.out.println(num1B.intValue());
+        System.out.println(myDouble.doubleValue());
+        System.out.println(myFloat.floatValue());
+        System.out.println(myByte.byteValue());
+        storeData(num1);
+
+        int age = Integer.parseInt("37");
+        double amount = Double.parseDouble("345.123");
+        boolean flag = Boolean.parseBoolean("false");
+    }
+
+    public static void storeData(Object obj) {
+
+    }
+}
