d8b4965fb023f2608cc0ab14382b9ac203ff733e
chiahsingchu <chiahsingtony@gmail.com>
2023-08-13 20:31:10 -0700

    Add unit test example

---
+++ src/main/java/com/codingjx/fundamental/test/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.codingjx.fundamental.test.calculator;
+
+public class Calculator {
+    public int add(int num1, int num2) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/codingjx/fundamental/test/calculator/CalculatorTest.java
@@ -0,0 +1,14 @@
+package com.codingjx.fundamental.test.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+    @Test
+    public void canAddZeroPlusZero() {
+        Calculator calc = new Calculator();
+        int sum = calc.add(0, 0);
+        assertEquals(0, sum);
+    }
+}
