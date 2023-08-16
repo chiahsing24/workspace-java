5f4f85a80ed5349dff8a39d777c173f8044cf54a
chiahsingchu <chiahsingtony@gmail.com>
2023-08-15 18:13:37 -0700

    Add how to use setUp method for unit test

--- src/test/java/com/codingjx/fundamental/test/calculator/CalculatorTest.java
+++ src/test/java/com/codingjx/fundamental/test/calculator/CalculatorTest.java
@@ -1,14 +1,28 @@
 package com.codingjx.fundamental.test.calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
+
+    private Calculator calc;
+
+    @BeforeEach
+    void setUp() {
+        calc = new Calculator();
+    }
+
     @Test
     public void canAddZeroPlusZero() {
-        Calculator calc = new Calculator();
         int sum = calc.add(0, 0);
         assertEquals(0, sum);
+    }
+
+    @Test
+    public void canAddOnePlusOne() {
+        int sum = calc.add(1, 1);
+        assertEquals(sum, 2);
     }
 }
