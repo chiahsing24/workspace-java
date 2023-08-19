47c186c14c9265051849a5bac409135536ebcb62
chiahsingchu <chiahsingtony@gmail.com>
2023-08-18 21:14:26 -0700

    Intro to Enum usage

---
+++ src/main/java/com/codingjx/fundamental/oop/terrymartin/blackjack/Rank.java
@@ -0,0 +1,17 @@
+package com.codingjx.fundamental.oop.terrymartin.blackjack;
+
+public enum Rank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
---
+++ src/main/java/com/codingjx/fundamental/oop/terrymartin/blackjack/Suit.java
@@ -0,0 +1,8 @@
+package com.codingjx.fundamental.oop.terrymartin.blackjack;
+
+public enum Suit {
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES
+}
