314177a6aac4cc102990812534f836bf53dc004c
chiahsingchu <chiahsingtony@gmail.com>
2023-08-18 21:35:21 -0700

    Intro to Enum field

---
+++ src/test/java/com/codingjx/fundamental/oop/terrymartin/blackjack/SuitTest.java
@@ -0,0 +1,12 @@
+package com.codingjx.fundamental.oop.terrymartin.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuitTest {
+    @Test
+    void heartPrintsHeart() {
+        assertEquals("\u2665", Suit.HEARTS.toString());
+    }
+}
