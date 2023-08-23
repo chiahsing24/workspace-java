1ba0d4d37dc970230273d9973a51d51a280c2597
chiahsingchu <chiahsingtony@gmail.com>
2023-08-22 19:57:59 -0700

    Cleanup project

--- src/main/java/com/codingjx/fundamental/string/terrymartin/Demo08.java
+++
@@ -1,9 +0,0 @@
-package com.codingjx.fundamental.string.terrymartin;
-
-public class Demo08 {
-    public static void main(String[] args) {
-        String myText = "Four score and seven years ago";
-        System.out.println(myText.contains("seven"));
-
-    }
-}
