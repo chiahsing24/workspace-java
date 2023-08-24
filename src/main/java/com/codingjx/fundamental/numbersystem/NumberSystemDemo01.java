154bdbaf0862da54b05aa968d56aee0f60e8466d
chiahsingchu <chiahsingtony@gmail.com>
2023-08-23 19:53:58 -0700

    Add number system demo

---
+++ src/main/java/com/codingjx/fundamental/numbersystem/NumberSystemDemo01.java
@@ -0,0 +1,17 @@
+package com.codingjx.fundamental.numbersystem;
+
+public class NumberSystemDemo01 {
+    public static void main(String[] args) {
+        // 十進制
+        int num1 = 103;
+        System.out.println("num1: " + num1);
+
+        // 二進制
+        int num2 = 0b10;
+        System.out.println("num2: " + num2);
+
+        // 十六進制
+        int num3 = 0X23a;
+        System.out.println("num3: " + num3);
+    }
+}
