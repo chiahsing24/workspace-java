bfb4fc7e1f82327f8be677b8858f8548fa873a77
chiahsingchu <chiahsingtony@gmail.com>
2023-08-23 19:59:24 -0700

    Add bit manipulation demo

---
+++ src/main/java/com/codingjx/fundamental/bitmanipulation/BitManipulationDemo01.java
@@ -0,0 +1,17 @@
+package com.codingjx.fundamental.bitmanipulation;
+
+public class BitManipulationDemo01 {
+    public static void main(String[] args) {
+        int num1 = 7;
+        System.out.println("num1 << 1: " + (num1 << 1));
+        System.out.println("num1 << 2: " + (num1 << 2));
+        System.out.println("num1 << 3: " + (num1 << 3));
+        System.out.println();
+
+        int num2 = -7;
+        System.out.println("num2 << 1: " + (num2 << 1));
+        System.out.println("num2 << 2: " + (num2 << 2));
+        System.out.println("num2 << 3: " + (num2 << 3));
+        System.out.println();
+    }
+}
