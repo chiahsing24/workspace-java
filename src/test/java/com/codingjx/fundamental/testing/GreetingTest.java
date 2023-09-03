927844dbc2e121499b54e505e807245f4d0cf60b
chiahsingchu <chiahsingtony@gmail.com>
2023-09-03 15:33:02 -0700

    Demo @BeforeAll, @BeforeEach, @AfterEach, @AfterAll

--- src/test/java/com/codingjx/fundamental/testing/GreetingTest.java
+++ src/test/java/com/codingjx/fundamental/testing/GreetingTest.java
@@ -1,16 +1,30 @@
 package com.codingjx.fundamental.testing;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
     Greeting greeting;
 
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("@BeforeAll -- I am only called once!");
+    }
+
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("@AfterAll -- I am only called once!");
+    }
+
     @BeforeEach
     void setUp() {
         greeting = new Greeting();
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("@AfterEach -- I am called every time!");
     }
 
     @Test
